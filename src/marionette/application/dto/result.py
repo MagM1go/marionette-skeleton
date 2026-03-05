e1fb21202813fc7f38a5277dc47ff74da10cfa6e
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-03-05 12:20:05 +0700

    Delete .gitkeep, result_presenter and result + embed.py

--- src/marionette/application/dto/result.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from marionette.application.dto.embed import Embed
-
-
-@dataclass
-class Result:
-    content: str | None = None
-    embed: Embed | None = None
